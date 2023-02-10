90646368efbbb26b9b6a67e41e5a531b861d0e07
Aschen <amaret@kuzzle.io>
2023-02-10 11:32:10 +0100

    prettier

--- apps/api/lib/modules/permissions/PermissionsModule.ts
+++ apps/api/lib/modules/permissions/PermissionsModule.ts
@@ -5,13 +5,14 @@ import { Module } from "../shared";
 
 export class PermissionsModule extends Module {
   register(): void {
-    const multiTenancy = this.app.plugin.get<MultiTenancyPlugin>("multi-tenancy");
+    const multiTenancy =
+      this.app.plugin.get<MultiTenancyPlugin>("multi-tenancy");
 
     multiTenancy.registerProfilesTemplates("air_quality", {
       [ProfileTenantAdmin.name]: ProfileTenantAdmin.definition,
       [ProfileTenantReader.name]: ProfileTenantReader.definition,
     });
-    }
+  }
 
   async start(): Promise<void> {
     // nothing there
