75877d1de4f50c403625a78357fe0a817ed42989
Sébastien Rodriguez <sebtiz13@gmail.com>
2023-02-20 15:41:46 +0100

    update tsconfig setup

--- apps/web/vue.config.js
+++ apps/web/vue.config.js
@@ -1,13 +1,20 @@
 const { defineConfig } = require('@vue/cli-service');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const TsconfigPaths = new TsconfigPathsPlugin({
+  // Use custom `tsconfig.json` if is defined. Usefull to improve/extends paths resolving for repositories that use this app like development application
+  configFile: process.env.TS_CONFIG,
   extensions: ['.ts', '.tsx', '.js', '.vue'],
 });
 
 module.exports = defineConfig({
-  productionSourceMap: process.env.NODE_ENV === 'development',
+  chainWebpack(config) {
+    config.plugin('fork-ts-checker').tap((options) => {
+      options[0].typescript.configFile = process.env.TS_CONFIG;
+      return options;
+    });
+  },
   configureWebpack: {
-    devtool: process.env.NODE_ENV === 'development' ? 'eval-source-map' : undefined,
+    devtool: process.env.NODE_ENV === 'development' ? 'eval-source-map' : 'source-map',
     resolve: {
       plugins: [TsconfigPaths],
     },
