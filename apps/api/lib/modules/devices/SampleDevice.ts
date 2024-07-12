0409817ff5d8c00717d13a24d32ce133c3de2af1
Juiced66 <juliendecosse30630@gmail.com>
2024-07-12 10:55:31 +0200

    feat: support metadata groups and detail for ExampleAsset and SampleDevice

--- apps/api/lib/modules/devices/SampleDevice.ts
+++ apps/api/lib/modules/devices/SampleDevice.ts
@@ -14,9 +14,9 @@ export interface SampleDeviceMetadata extends Metadata {
 }
 
 /**
- * Type meant to be used when manipulating an asset.
+ * Type meant to be used when manipulating a device.
  *
- * It is constructed when the types of the asset possible measures and metadata.
+ * It is constructed when the types of the device possible measures and metadata.
  *
  * This is optional and can be omitted if you don't want strong typing
  */
@@ -25,6 +25,10 @@ export interface SampleDeviceContent
   model: 'SampleDevice';
 }
 
+/**
+ * Device definition used by the plugin to create associated resources
+ * and update mappings.
+ */
 export const SampleDeviceDefinition: DeviceModelDefinition = {
   decoder: new SampleDeviceDecoder(),
   defaultMetadata: {
@@ -33,5 +37,47 @@ export const SampleDeviceDefinition: DeviceModelDefinition = {
   metadataMappings: {
     trackerType: { type: 'keyword' },
     serialNumber: { type: 'keyword' },
+  },
+  metadataDetails: {
+    trackerType: {
+      group: 'deviceInfo',
+      locales: {
+        en: {
+          friendlyName: "Tracker Type",
+          description: "The type of the tracker",
+        },
+        fr: {
+          friendlyName: "Type de Tracker",
+          description: "Le type de tracker",
+        },
+      },
+    },
+    serialNumber: {
+      group: 'deviceInfo',
+      locales: {
+        en: {
+          friendlyName: "Serial Number",
+          description: "The serial number of the device",
+        },
+        fr: {
+          friendlyName: "Numéro de Série",
+          description: "Le numéro de série de l'appareil",
+        },
+      },
+    },
+  },
+  metadataGroups: {
+    deviceInfo: {
+      locales: {
+        en: {
+          groupFriendlyName: "Device Information",
+          description: "Information about the device",
+        },
+        fr: {
+          groupFriendlyName: "Informations sur l'appareil",
+          description: "Informations sur l'appareil",
+        },
+      },
+    },
   },
 };
--- apps/api/lib/modules/tenant_exemple/assets/ExempleAsset.ts
+++ apps/api/lib/modules/tenant_exemple/assets/ExempleAsset.ts
@@ -9,7 +9,7 @@ import {
 export const assetModelName = 'ExempleAsset';
 
 /**
- * Type representing the metadata of a "Room" asset.
+ * Type representing the metadata of an "ExempleAsset".
  *
  * This is optional and can be omitted if you don't want strong typing
  */
@@ -23,7 +23,7 @@ export interface ExempleAssetMetadata extends Metadata {
 }
 
 /**
- * Type representing the measures of a "Room" asset.
+ * Type representing the measures of an "ExempleAsset".
  *
  * This is optional and can be omitted if you don't want strong typing
  */
@@ -33,7 +33,7 @@ export type ExempleAssetMeasurements = {
 };
 
 /**
- * Type meant to be used when manipulating a "Room" asset.
+ * Type meant to be used when manipulating an "ExempleAsset".
  *
  * It is constructed when the types of the asset possible measures and metadata.
  *
@@ -45,11 +45,15 @@ export interface ExempleAssetContent
 }
 
 /**
- * Asset definition used by the plugin to create associated ressources
- * and updates mappings.
+ * Asset definition used by the plugin to create associated resources
+ * and update mappings.
  */
 export const exempleAssetDefinition: AssetModelDefinition = {
-  defaultMetadata: {},
+  defaultMetadata: {
+    aStringMetadata: 'Default String',
+    position: { lat: 0, lon: 0 },
+    aNumberMetadata: 0,
+  },
   measures: [
     {
       name: 'temperature',
@@ -64,5 +68,72 @@ export const exempleAssetDefinition: AssetModelDefinition = {
     aStringMetadata: { type: 'keyword' },
     position: { type: 'geo_point' },
     aNumberMetadata: { type: 'float' },
+  },
+  metadataDetails: {
+    aStringMetadata: {
+      group: 'exempleGroup',
+      locales: {
+        en: {
+          friendlyName: "String Metadata",
+          description: "A string type metadata",
+        },
+        fr: {
+          friendlyName: "Métadonnées de chaîne de caractères",
+          description: "Une métadonnée de type chaîne de caractères",
+        },
+      },
+    },
+    aNumberMetadata: {
+      group: 'exempleGroup',
+      locales: {
+        en: {
+          friendlyName: "Number Metadata",
+          description: "A number type metadata",
+        },
+        fr: {
+          friendlyName: "Métadonnées de nombre",
+          description: "Une métadonnée de type nombre",
+        },
+      },
+    },
+    position: {
+      group: 'location',
+      locales: {
+        en: {
+          friendlyName: "Position",
+          description: "Geographical position",
+        },
+        fr: {
+          friendlyName: "Position",
+          description: "Position géographique",
+        },
+      },
+    },
+  },
+  metadataGroups: {
+    exempleGroup: {
+      locales: {
+        en: {
+          groupFriendlyName: "Exemple Group",
+          description: "Exemple group data",
+        },
+        fr: {
+          groupFriendlyName: "Groupe Exemple",
+          description: "Données du groupe exemple",
+        },
+      },
+    },
+    location: {
+      locales: {
+        en: {
+          groupFriendlyName: "Location",
+          description: "Location data",
+        },
+        fr: {
+          groupFriendlyName: "Localisation",
+          description: "Données de localisation",
+        },
+      },
+    },
   },
 };
