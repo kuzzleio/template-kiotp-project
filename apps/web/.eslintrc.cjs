831d8cdb18ddc4f94f607a51111d9b61df129584
Ricky <n.riquelmebareiro@gmail.com>
2025-01-15 09:08:16 +0100

    chore: Update deps (#15)
    * chore: Update deps
    
    * chore: update package-lock.json
    
    * chore: revert eslint-plugin to be compatible with frontend
    
    * chore: Update eslint config

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -2,16 +2,15 @@ const path = require('node:path');
 
 module.exports = {
   root: true,
-  extends: ['plugin:vue-kuzzle/default'],
+  extends: ['plugin:vue-kuzzle/default', 'plugin:vue-kuzzle/base', 'plugin:vue-kuzzle/typescript'],
   parserOptions: {
     project: path.join(__dirname, 'tsconfig.test.json'),
   },
   rules: {
-    // TODO refactor to enable these rules
-    // ? (https://github.com/vuejs/eslint-plugin-vue/issues/2259)
     'vue/no-setup-props-destructure': 'off',
     'vue/multi-word-component-names': 'off',
     '@typescript-eslint/consistent-type-imports': 'off',
+    '@typescript-eslint/restrict-plus-operands': ['off'],
   },
   ignorePatterns: ['dist/*'],
 };
