d6ee4a904f5936012ecd4368308dc766506c97f0
Alexandre Bouthinon <bouthinon.alexandre@gmail.com>
2025-05-21 16:35:01 +0200

    test: add a sample connection test to Kuzzle (#18)
    * test: add a sample connection test to Kuzzle
    
    * fix: prefer server:publicApi usage for the connection sample test

---
+++ apps/api/tests/scenarios/sample/Connect.test.ts
@@ -0,0 +1,19 @@
+import { Kuzzle, Http } from 'kuzzle-sdk';
+
+describe('Connect', () => {
+  let kuzzle: Kuzzle;
+
+  beforeAll(async () => {
+    kuzzle = new Kuzzle(new Http('localhost'));
+    await kuzzle.connect();
+  });
+
+  it('should connect to Kuzzle backend', async () => {
+    const serverInfo = kuzzle.query({
+      action: 'publicApi',
+      controller: 'server',
+    });
+
+    await expect(serverInfo).not.toThrow();
+  });
+});
